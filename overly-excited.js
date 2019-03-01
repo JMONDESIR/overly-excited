d9015297568720ca4206401d4312405a8cbac263
JMONDESIR <Joey.M.Dev@gmail.com>
2019-03-01 11:10:31 -0600

    exclamation point on %3===0 words

--- overly-excited.js
+++ overly-excited.js
@@ -3,12 +3,15 @@ let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the
 function addExcitement(theWordArray) {
         // Each time the for loop executes, you're going to add one more word to this string
         let buildMeUp = ""
+
         for (let i = 0; i < theWordArray.length; i++) {
-                // Concatenate the new word onto buildMeUp
-                buildMeUp = buildMeUp + " " + theWordArray[i]
-                // Print buildMeUp to the console
-                console.log(buildMeUp)
+                if (i % 3 === 0 && i !== 0) {
+                        buildMeUp = buildMeUp + " " + theWordArray[i] + "!";
+                } else {
+                        buildMeUp = buildMeUp + " " + theWordArray[i];
+                        console.log(buildMeUp);
+                        // Print buildMeUp to the console
+                }
         }
 }
-// Invoke the function and pass in the array
 addExcitement(sentence)
